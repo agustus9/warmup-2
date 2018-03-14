423ba0bc8b19df85987b8abc6de70b0722d47401
Mack <amack@proforma.com>
2018-03-14 09:17:18 -0400

    added querySelector

--- public/main.js
+++ public/main.js
@@ -1,5 +1,18 @@
+function rand(start, end) {
+  let r = start + Math.floor(Math.random() * (end - start));
+  return r;
+}
+
+
+function backgroundColor() {
+  let bgColor = "rgb(" + rand(0, 255) + "," + rand(0, 255) + "," + rand(0, 255) + ")";
+  document.querySelector("body").style.backgroundColor = bgColor;
+  
+
+}
+
 const main = () => {
-  document.querySelector('h1').textContent += '?'
+  document.querySelector('h1').textContent += ''
 }
 
 document.addEventListener('DOMContentLoaded', main)
